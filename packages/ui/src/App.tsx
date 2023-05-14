bb379d7a2cc343520e1f1b39de8f57ede482b04f
glocore <24620401+glocore@users.noreply.github.com>
2023-05-14 23:01:31 +0530

    add searchbox

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -3,7 +3,7 @@ import {
   createController,
   createRemoteReceiver,
 } from "@remote-ui/react/host";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { ButtonProps } from "remote-components";
 
 const App = () => {
@@ -17,10 +17,14 @@ const App = () => {
   }, [receiver]);
 
   return (
-    <>
+    <div className="text-white">
+      <input
+        placeholder="Command-a-lot"
+        className="w-full text-white placeholder:text-gray-500 bg-transparent border-b border-gray-700 px-4 py-3 text-lg outline-none"
+      />
       <p>hello from react</p>
       <RemoteRenderer receiver={receiver} controller={controller} />
-    </>
+    </div>
   );
 };
 export default App;
