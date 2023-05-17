82ce36300397dee0ea11c18a9eb68a60dcde4ed5
glocore <24620401+glocore@users.noreply.github.com>
2023-05-17 22:38:56 +0530

    add actions panel

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -3,9 +3,12 @@ import {
   createController,
   createRemoteReceiver,
 } from "@remote-ui/react/host";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { ButtonProps } from "remote-components";
 import { useWindowFocus } from "./hooks/useWindowFocus";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import tinykeys from "tinykeys";
+import { cn } from "./utils";
 
 const App = () => {
   const receiver = useMemo(() => createRemoteReceiver(), []);
@@ -38,25 +41,70 @@ const Button = (props: ButtonProps) => {
 
 function Footer() {
   const isWindowFocused = useWindowFocus();
+  const [isActionsOpen, setIsActionsOpen] = useState(false);
+
+  useEffect(() => {
+    let unsubscribe = tinykeys(window, {
+      "$mod+K": () => setIsActionsOpen((o) => !o),
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
 
   return (
-    <div className="bg-neutral-800/60 border-t border-neutral-500/50 flex justify-between px-2 py-1.5">
+    <div className="bg-neutral-800/60 border-t border-neutral-500/50 flex justify-between px-2 py-1.5 enable-window-drag">
       <div />
 
       <div>
-        <button
-          className={`text-sm font-semibold rounded-md outline-none px-2 py-0.5 pr-0.5 ${
-            isWindowFocused ? "text-neutral-400" : "text-neutral-500"
-          } active:text-neutral-300 hover:bg-neutral-700 active:bg-neutral-600 transition-colors duration-75`}
+        <DropdownMenu.Root
+          onOpenChange={(open) => setIsActionsOpen(open)}
+          open={isActionsOpen}
         >
-          <span className="pr-2">Actions</span>
-          <kbd className="font-sans bg-neutral-700 rounded inline-block w-6 h-6 leading-6 mr-0.5">
-            ⌘
-          </kbd>
-          <kbd className="font-sans bg-neutral-700 rounded inline-block w-6 h-6 leading-6">
-            K
-          </kbd>
-        </button>
+          <DropdownMenu.Trigger asChild>
+            <button
+              className={cn(
+                "text-sm font-semibold rounded-md outline-none px-2 py-0.5 pr-0.5 hover:bg-neutral-700 transition-colors duration-75",
+                {
+                  "text-neutral-400": isWindowFocused,
+                  "text-neutral-500": !isWindowFocused,
+                  "text-neutral-300": isActionsOpen,
+                  "bg-neutral-600": isActionsOpen,
+                }
+              )}
+            >
+              <span className="pr-2 select-none transition-colors duration-75">
+                Actions
+              </span>
+              <kbd
+                className={cn(
+                  "font-sans bg-neutral-700 rounded inline-block w-6 h-6 leading-6 mr-0.5 select-none transition-colors duration-75",
+                  {
+                    "bg-neutral-600": isActionsOpen,
+                    "text-neutral-300": isActionsOpen,
+                  }
+                )}
+              >
+                ⌘
+              </kbd>
+              <kbd
+                className={cn(
+                  "font-sans bg-neutral-700 rounded inline-block w-6 h-6 leading-6 select-none transition-colors duration-75",
+                  {
+                    "bg-neutral-600": isActionsOpen,
+                    "text-neutral-300": isActionsOpen,
+                  }
+                )}
+              >
+                K
+              </kbd>
+            </button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content className="h-40 w-80 -top-4 right-2 relative rounded-lg bg-neutral-800/50 border border-neutral-500/50 shadow-lg shadow-neutral-900"></DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
       </div>
     </div>
   );
