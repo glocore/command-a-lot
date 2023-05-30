d76c1740ca281cc5676fea515d17b4d910910c27
glocore <24620401+glocore@users.noreply.github.com>
2023-05-30 08:34:45 +0530

    fix tests

--- packages/workflow-engine/src/Runtime.spec.ts
+++ packages/workflow-engine/src/Runtime.spec.ts
@@ -1,82 +1,94 @@
 import { describe, expect, it, vi } from "vitest";
-import { Task } from "./Task";
-import { TaskMessage, WorkflowJson, WorkflowNode } from "./types";
-import { Workflow } from "./Workflow";
-import { Runtime } from "./Runtime";
 import { Context } from "./Context";
+import { Runtime } from "./Runtime";
+import { Task } from "./Task";
+import { Workflow } from "./Workflow";
+import { WorkflowJson } from "./types";
 
 describe("Runtime", () => {
-  it("runs a simple workflow", () => {
-    const workflowJson: WorkflowJson = {
-      version: 1.0,
-      name: "Yell text",
-      nodes: [
-        {
-          kind: "task",
-          id: "1",
-          name: "1",
-          task: "to-upper-case-task",
-          args: {
-            text: "hello world",
+  it("runs a simple workflow", () =>
+    new Promise<void>((res, rej) => {
+      const workflowJson: WorkflowJson = {
+        version: 1.0,
+        name: "Yell text",
+        nodes: [
+          {
+            kind: "task",
+            id: "1",
+            name: "1",
+            task: "to-upper-case-task",
+            args: {
+              text: "hello world",
+            },
+            next: "2",
           },
-          next: "2",
-        },
-        {
-          kind: "task",
-          id: "2",
-          name: "2",
-          task: "append-exclamation-task",
-        },
-      ],
-    };
+          {
+            kind: "task",
+            id: "2",
+            name: "2",
+            task: "append-exclamation-task",
+          },
+        ],
+      };
 
-    const tasks = new Map<string, Task>();
-    tasks.set("to-upper-case-task", new ToUpperCaseTask());
-    tasks.set("append-exclamation-task", new AppendExclamationTask());
+      const tasks = new Map<string, Task>();
+      tasks.set("to-upper-case-task", new ToUpperCaseTask());
+      tasks.set("append-exclamation-task", new AppendExclamationTask());
 
-    const workflow = new Workflow(workflowJson);
-    const context = new Context();
-    const runtime = new Runtime({ workflow, tasks, context });
+      const workflow = new Workflow(workflowJson);
+      const context = new Context();
+      const runtime = new Runtime({ workflow, tasks, context });
 
-    const onStart = vi.fn();
-    const onEnd = (message: unknown) => {
-      expect(message).toEqual({ message: { text: "HELLO WORLD!" } });
-    };
-    runtime.on("start", onStart);
-    runtime.on("end", onEnd);
+      const onStart = vi.fn();
+      const onEnd = (message: unknown) => {
+        try {
+          expect(message).toEqual({ message: { text: "HELLO WORLD!" } });
+          res();
+        } catch (error) {
+          rej(error);
+        }
+      };
+      runtime.on("start", onStart);
+      runtime.on("end", onEnd);
 
-    runtime.start();
-    expect(onStart).toBeCalledWith({ node: workflow.getNode("1") });
-  });
+      runtime.start();
+      expect(onStart).toBeCalledWith({ node: workflow.getNode("1") });
+    }));
 
-  it("updates a variable", () => {
-    const workflowJson: WorkflowJson = {
-      version: 1.0,
-      name: "Add two",
-      variables: { $number: 1 },
-      nodes: [
-        {
-          kind: "task",
-          id: "1",
-          name: "1",
-          task: "add-two-task",
-        },
-      ],
-    };
+  it("updates a variable", () =>
+    new Promise<void>((res, rej) => {
+      const workflowJson: WorkflowJson = {
+        version: 1.0,
+        name: "Add two",
+        variables: { $number: 1 },
+        nodes: [
+          {
+            kind: "task",
+            id: "1",
+            name: "1",
+            task: "add-two-task",
+          },
+        ],
+      };
 
-    const tasks = new Map<string, Task>();
-    tasks.set("add-two-task", new AddTwoTask());
+      const tasks = new Map<string, Task>();
+      tasks.set("add-two-task", new AddTwoTask());
 
-    const workflow = new Workflow(workflowJson);
-    const context = new Context({ variables: workflowJson.variables });
-    const runtime = new Runtime({ workflow, tasks, context });
+      const workflow = new Workflow(workflowJson);
+      const context = new Context({ variables: workflowJson.variables });
+      const runtime = new Runtime({ workflow, tasks, context });
 
-    runtime.on("end", () => {
-      expect(context).toEqual({ variables: { $number: 3 } });
-    });
+      runtime.on("end", () => {
+        try {
+          expect(context).toEqual({ variables: { $number: 3 } });
+          res();
+        } catch (error) {
+          rej(error);
+        }
+      });
 
-    runtime.start();
-  });
+      runtime.start();
+    }));
 });
 
 class ToUpperCaseTask extends Task {
@@ -97,8 +109,8 @@ class AppendExclamationTask extends Task {
 
 class AddTwoTask extends Task {
   run: Task["run"] = async ({ node, message, context }) => {
-    if (typeof context.variables.number === "number") {
-      context.variables.number += 2;
+    if (typeof context.variables.$number === "number") {
+      context.variables.$number += 2;
     }
 
     return {};
